c5f89ed13004652277bd1a43467e8f5795701368
Samisha <samishak62@gmail.com>
2020-07-15 20:36:46 +0530

    social card completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import SocialCard from './SocialCard.js'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SocialCard />
   </React.StrictMode>,
   document.getElementById('root')
 );
